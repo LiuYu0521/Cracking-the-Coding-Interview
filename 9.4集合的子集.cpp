b6312ae1156c7864c0572f5b70a1e80234449101
LiuYu0521 <ly819933137@gmail.com>
2017-08-18 15:59:32 +0800

    利用数的递减和二进制每位关系

--- 9.4集合的子集.cpp
+++ 9.4集合的子集.cpp
@@ -1,6 +1,19 @@
 class Subset {
 public:
-    vector<vector<int> > getSubsets(vector<int> A, int n) {
-        // write code here
-    }
+    vector<vector<int> > getSubsets(vector<int> A, int n) {
+        // write code here
+        sort(A.begin(), A.end());
+        int size = 1 << n;
+        vector<vector<int> >subsets;
+        for (int i = size - 1; i > 0; --i) {
+            vector<int> subset;
+            for (int j = n - 1; j >= 0; --j) {
+                if ((i >> j) & 1) {
+                    subset.push_back(A[j]);
+                }
+            }
+            subsets.push_back(subset);
+        }
+        return subsets;
+    }
 };
